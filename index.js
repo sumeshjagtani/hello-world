9320b0fcf0efca3e73f1ff3d013dcb27a80ddd5e
Sumesh Jagtani <sumeshjagtani@Sumeshs-MBP.home>
2018-02-12 17:20:19 -0500

    V1 Changes

--- index.js
+++ index.js
@@ -15,28 +15,8 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-});
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
+app.get('/zeit', function(request, response) {
+  response.render('pages/zeit')
 });
 
 app.listen(app.get('port'), function() {
